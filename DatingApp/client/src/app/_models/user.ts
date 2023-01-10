aa0e9b47b7a64a80c5654ab11b1cf1dab9562848
k5-salman <salmankhalid518@gmail.com>
2023-01-10 15:59:42 +0500

    End of ssection 9

--- DatingApp/client/src/app/_models/user.ts
+++ DatingApp/client/src/app/_models/user.ts
@@ -2,3 +2,29 @@ export interface User {
     username : string;
     token : string ;
 }
+
+// Using typeScripting
+// let data : number | string = "42";
+
+// data = 10;
+
+// interface Car {
+//     color : String;
+//     model : string;
+//     topSpeed? : number;
+// }
+
+// const car1 : Car = {
+//     color : "blue",
+//     model : "BMW"
+// }
+
+// const car2 = {
+//     color : "red",
+//     model : "Mercedes",
+//     topSpeed : 200
+// }
+
+// const multiply = (x : number,y: number) => {
+//     x * y ;
+// }
