17acb15a38668bd6a87a94414404be04c093c9bb
k5-salman <salmankhalid518@gmail.com>
2023-01-25 21:30:12 +0500

    End of section 13

---
+++ DatingApp/API/Helpers/LogUserActivity.cs
@@ -0,0 +1,24 @@
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultcontext = await next();
+
+            if (!resultcontext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userId = resultcontext.HttpContext.User.GetUserId();
+
+            var repo = resultcontext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+
+            var user = await repo.GetUserByIdAsync(int.Parse(userId));
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
+    }
+}
